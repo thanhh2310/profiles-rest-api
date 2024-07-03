3f616530b5a5da3419567381070b67fd83eaa660
Thanh <tienthanhv37@gmail,com>
2024-07-04 01:13:26 +0700

    Start with APIView with method get

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Su that', 'luonn', 'chi', 'co mot',
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
